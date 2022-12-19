ce0da87da0f4ff5aca4b9c42dff6713244bc5a22
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-12-19 22:36:52 +0530

    clear

--- rpstrackerng/src/app/app.module.ts
+++ rpstrackerng/src/app/app.module.ts
@@ -1,22 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module';
-import { BacklogModule } from './modules/backlog/backlog.module';
-import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { CoreModule } from "./core/core.module";
+import { BacklogModule } from "./modules/backlog/backlog.module";
+import { DashboardModule } from "./modules/dashboard/dashboard.module";
 
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { ButtonsModule } from '@progress/kendo-angular-buttons';
-
+import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,9 +22,8 @@ import { ButtonsModule } from '@progress/kendo-angular-buttons';
     DashboardModule,
     BacklogModule,
     BrowserAnimationsModule,
-    ButtonsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
